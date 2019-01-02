8dd6783355c7bfe5271de25295346168c0e3d911
Jack Vaught <johnmvaught@gmail.com>
2019-01-02 11:30:50 -0800

    hacked together placeholder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="center">
+        <div className="title">
+          <div className="name">jackvaught</div>
+          <div className="about">software engineer</div>
+        </div>
+        <div className="buttons">
+          <a className="button" href={'https://twitter.com/Jack_Vaught'}>twitter</a>
+          <a className="button" href={'https://github.com/jacten'}>github</a>
+        </div>
       </div>
     );
   }
