27b033f0fa193aaa26d7e774de3682a1308a4c9b
Jack Vaught <johnmvaught@gmail.com>
2019-01-03 14:02:50 -0800

    modifies basic file structure adds reactrouter

---
+++ src/Components/Landing/Landing.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="center">
+        <div className="title">
+          <div className="name">jackvaught</div>
+          <div className="about">software engineer</div>
+        </div>
+        <div className="buttons">
+          <a className="button" href={'https://twitter.com/Jack_Vaught'}>twitter</a>
+          <a className="button" href={'https://github.com/jacten'}>github</a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
