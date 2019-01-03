c0f220bc839b2fda1d992b7f2560a6657037fc9e
Jack Vaught <johnmvaught@gmail.com>
2019-01-03 14:04:13 -0800

    adds Landing page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
+import Landing from './Components/Landing/Landing';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="center">
-        <div className="title">
-          <div className="name">jackvaught</div>
-          <div className="about">software engineer</div>
-        </div>
-        <div className="buttons">
-          <a className="button" href={'https://twitter.com/Jack_Vaught'}>twitter</a>
-          <a className="button" href={'https://github.com/jacten'}>github</a>
-        </div>
-      </div>
+      <Landing/>
     );
   }
 }
--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './Landing.css';
 
 class Landing extends Component {
   render() {
