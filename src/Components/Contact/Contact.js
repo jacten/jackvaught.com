7db3454a085e5abe764a09348d1e6b9c6b8a3861
Jack Vaught <johnmvaught@gmail.com>
2019-01-13 17:55:33 -0800

    adds contact pages v1

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -5,6 +5,19 @@ import email from '../../Assets/email.png';
 import s from './contact.module.css';
 
 class Contact extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { 
+      copySuccess: '',
+    }
+  }
+
+  copyToClipboard = (e) => {
+    this.textArea.select();
+    document.execCommand('copy');
+    this.setState({ copySuccess: 'copied!' });
+  };
   
   render() {
     return (
@@ -12,9 +25,22 @@ class Contact extends Component {
         <div className={s.title}>
           contact
         </div>
-        <div className={s.body}>
+        <form>
+          <textarea
+            className={s.textarea}
+            ref={(textarea) => this.textArea = textarea}
+            value='johnmvaught@gmail.com'
+           />
+         </form>
+        <div className={s.body} onClick={this.copyToClipboard}>
+          {/* <div>
+             {this.state.copySuccess}
+           </div> */}
           <span className={s.helper}/>
-          <img className={s.img} src={email} alt={"email me!"}/>
+          <div className={s.imgbox}>
+            <img className={s.img} src={email} alt={"email me!"}/>
+            <span className={s.email}>johnmvaught@gmail.com</span>
+          </div>
         </div>
       </div>
     )
